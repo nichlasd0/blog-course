147d7616132fddf5fdf76ca9fc42225bb3608883
Nichlas <nichlasd@gmail.com>
2021-10-20 15:56:29 +0200

    add title to pages with meta tags, also add SSR

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Article } from '../article';
 import { ArticleService } from '../article.service';
+import { SharedService } from '../shared.service';
 
 @Component({
   selector: 'app-article',
@@ -14,7 +16,10 @@ export class ArticleComponent implements OnInit {
 
   constructor(private route: ActivatedRoute,
     private articleService: ArticleService,
-    private router: Router) { }
+    private router: Router,
+    private titleService: Title,
+    private sharedService: SharedService,
+    private meta: Meta) { }
 
   ngOnInit() {
     this.route.params.subscribe(params => {
@@ -27,7 +32,30 @@ export class ArticleComponent implements OnInit {
           return; 
         }
         this.article = article;
-        console.log(this.article);
+        this.titleService.setTitle(`${this.article.title} - ${this.sharedService.blogTitle}`);
+        this.meta.addTags([
+          {
+            name: 'description', content: this.article.description, 
+          },
+          { 
+            property: "og:title", content: `${this.article.title} - ${this.sharedService.blogTitle}` 
+          },
+          {
+            property: "og:type", content: "website"
+          },
+          {
+            property: "og:url", content: this.sharedService.baseUrl + this.article.key
+          },
+          {
+            property: "og:image", content: this.article.imageUrl
+          },
+          {
+            property: "og:description", content: this.article.description
+          },
+          {
+            property: "og:site_name", content: this.sharedService.blogTitle
+          },
+        ]);
       }
     );
   });
